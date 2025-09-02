76582f69deedafa8c1967a5e2a005f0e2a4e1329
zoomahelmy2050-gif <zoomahelmy2050@gmail.com>
2025-09-03 01:10:31 +0300

    Fix dependency injection - add AuthModule imports to migrations and sync modules

--- server/src/migrations/migrations.module.ts
+++ server/src/migrations/migrations.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MigrationsController } from './migrations.controller.js';
 import { MigrationsService } from './migrations.service.js';
 import { PrismaModule } from '../prisma/prisma.module.js';
+import { AuthModule } from '../auth/auth.module.js';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [MigrationsController],
   providers: [MigrationsService],
   exports: [MigrationsService],
--- server/src/sync/sync.module.ts
+++ server/src/sync/sync.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SyncController } from './sync.controller.js';
 import { SyncService } from './sync.service.js';
 import { PrismaModule } from '../prisma/prisma.module.js';
+import { AuthModule } from '../auth/auth.module.js';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [SyncController],
   providers: [SyncService],
   exports: [SyncService],
